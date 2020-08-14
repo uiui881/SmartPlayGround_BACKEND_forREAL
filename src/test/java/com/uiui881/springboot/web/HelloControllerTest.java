b120d347b7eb1837f6590721621432500f59bc56
uiui881 <33458092+uiui881@users.noreply.github.com>
2020-08-15 03:23:41 +0900

    로그인 기능 테스트까지 끝

--- src/test/java/com/uiui881/springboot/web/HelloControllerTest.java
+++ src/test/java/com/uiui881/springboot/web/HelloControllerTest.java
@@ -1,9 +1,14 @@
 package com.uiui881.springboot.web;
 
+import com.uiui881.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,12 +17,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {@ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="CHILD")
     @Test
     public void hello가_리턴된다() throws Exception{
         String hello ="hello";
@@ -25,6 +32,7 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="CHILD")
     @Test
     public void helloDto가_리턴된다() throws Exception{
         String name ="hello";
--- src/test/java/com/uiui881/springboot/web/PlaygroundsApiControllerTest.java
+++ src/test/java/com/uiui881/springboot/web/PlaygroundsApiControllerTest.java
@@ -1,26 +1,36 @@
 package com.uiui881.springboot.web;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uiui881.springboot.domain.playgrounds.Playgrounds;
 import com.uiui881.springboot.domain.playgrounds.PlaygroundsRepository;
 import com.uiui881.springboot.web.dto.PlaygroundsSaveRequestDto;
 import com.uiui881.springboot.web.dto.PlaygroundsUpdateRequestDto;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.
+        springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -35,12 +45,23 @@ public class PlaygroundsApiControllerTest {
     @Autowired
     private PlaygroundsRepository playgroundsRepository;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    }
+
     @After
     public void tearDown() throws Exception{
         playgroundsRepository.deleteAll();
     }
 
     @Test
+    @WithMockUser(roles="CHILD")
     public void Playgrounds_등록된다() throws Exception{
         String name ="놀이터1 이름";
         double minLatitude =1.0;
@@ -57,10 +78,9 @@ public class PlaygroundsApiControllerTest {
 
         String url ="http://localhost:"+port+"/api/v1/playgrounds";
 
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
-
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        mvc.perform(post(url).contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         List<Playgrounds> all = playgroundsRepository.findAll();
         assertThat(all.get(0).getName()).isEqualTo(name);
@@ -74,6 +94,7 @@ public class PlaygroundsApiControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="CHILD")
     public void Playgrounds_수정된다() throws Exception{
         Playgrounds savedPlaygrounds = playgroundsRepository.save(Playgrounds.builder().name("놀이터1 이름")
         .minLatitude(1).maxLatitude(3).minLongitude(1).maxLongitude(3).congestion(3).writer("김소림").build());
@@ -91,15 +112,15 @@ public class PlaygroundsApiControllerTest {
                 .congestion(expectedCongestion)
                 .build();
 
-        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+        String url = "http://localhost:" + port + "/api/v1/playgrounds/" + updateId;
 
-        //when
-        HttpEntity<PlaygroundsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+        //여기 post->put으로 바꿔야함
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
-        //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
         List<Playgrounds> all = playgroundsRepository.findAll();
         assertThat(all.get(0).getMinLatitude()).isEqualTo(expectedMinLatitude);
         assertThat(all.get(0).getMaxLatitude()).isEqualTo(expectedMaxLatitude);
